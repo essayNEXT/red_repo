baf5205f598ce3638311be8745a6ca7ebfde944b
Kostiantyn Zivenko <kos.zivenko@gmail.com>
2023-02-23 20:29:51 +0100

    checked: balck

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -2,13 +2,15 @@ from piccolo.conf.apps import AppRegistry
 from piccolo.engine.postgres import PostgresEngine
 
 
-DB = PostgresEngine(config={
-    "database": "",
-    "user": "",
-    "password": "",
-    "host": "",
-    "port": "",
-})
+DB = PostgresEngine(
+    config={
+        "database": "",
+        "user": "",
+        "password": "",
+        "host": "",
+        "port": "",
+    }
+)
 
 
 # A list of paths to piccolo apps
--- redbot/code/settings.py
+++ redbot/code/settings.py
@@ -3,11 +3,11 @@ import os
 
 from dotenv import load_dotenv
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARNING)
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.WARNING)
 load_dotenv()
 
 try:
-    RED_BOT_TOKEN = os.environ['RED_BOT_TOKEN']
+    RED_BOT_TOKEN = os.environ["RED_BOT_TOKEN"]
 except KeyError as err:
     logging.critical(f"Can't read token from environment variable. Message: {err}")
     raise KeyError(err)
