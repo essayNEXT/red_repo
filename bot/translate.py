439fc8c7215f32a206a1976208f124fc70d43c61
Viktoria <vikazenchenko13@gmail.com>
2023-03-02 17:11:29 +0200

    Changed the translation function

--- bot/translate.py
+++ bot/translate.py
@@ -1,5 +1,6 @@
 from os import environ
 from google.cloud import translate
+from google.cloud import translate_v2 as tr
 
 environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'C:/Users/vikaz/OneDrive/Рабочий стол/Work/Project/red_repo/translater-bot-intern-b9dec3a171bd.json'
 project_id = 'translater-bot-intern'
@@ -7,29 +8,43 @@ parent = f"projects/{project_id}"
 
 
 def translate_message(text):
+    # """Detects the text's language"""
+    # translate_client = tr.Client()
+    # result = translate_client.detect_language(text) 
+    # print("Language: {}".format(result["language"])) # Дивилася, наскільки правильно визначає мову
+
+    """Translate text"""
     client = translate.TranslationServiceClient()
-    target_language_code = "en"
-        
+            
     response = client.translate_text(
-        contents=[text], 
-        target_language_code=target_language_code,
-        parent=parent,
-        )
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",
+            # "source_language_code": , # Google автоматично визначає мову оригіналу; потрібно, якщо ми будемо додавати 
+                                        # функцію вибору мови з якої користувач хоче вчити.
+            "target_language_code": "en"  # Вказувати мову, на яку треба робити переклад обов'язково
+        }
+    )
     
     for translation in response.translations:
         return translation.translated_text
+    
+
+def list_languages(): 
+    """Lists all available languages."""
+
+    translate_client = tr.Client()
+
+    results = translate_client.get_languages()
+
+    for language in results:
+        print(u"{name} ({language})".format(**language))
 
 
-# def list_languages():
-#     """Lists all available languages."""
-#     from google.cloud import translate_v2 as translate
-
-#     translate_client = translate.Client()
-
-#     results = translate_client.get_languages(target_language=target)
-
-#     for language in results:
-#         print(u"{name} ({language})".format(**language))
+# if __name__ == '__main__':
+#     list_languages()
+    
 
 
 
